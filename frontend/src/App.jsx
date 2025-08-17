4d4ad9d05f26f10045860a6619fafe2038a618ab
dev-anvith <anvithrh@gmail.com>
2025-08-17 15:00:15 +0530

    Fix error in dependency

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
 import { SparklesIcon, ClipboardDocumentIcon, CheckIcon, XCircleIcon, TrashIcon, ShareIcon } from '@heroicons/react/24/outline';
-import ReactMarkdown from 'react-markdown';
 
 export default function App() {
   const [transcript, setTranscript] = useState("");
